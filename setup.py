9dbe61650037cffbb37bc8d79d5220d564e9a8b8
Scott Hajek <shajek@pivotal.io>
2018-12-07 18:26:07 -0500

    bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sql_magic",
-    version="0.0.3",
+    version="0.0.4",
     author='Chris Rawles',
     author_email='crawles@pivotal.io',
     packages=find_packages(),
--- sql_magic/__init__.py
+++ sql_magic/__init__.py
@@ -15,5 +15,5 @@
 # limitations under the License.
 
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 from .sql_magic import *
